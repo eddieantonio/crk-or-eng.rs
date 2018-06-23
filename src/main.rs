7a21ca3a6d44b155bf0a6d15fa4fa33c71225731
Eddie Antonio Santos <easantos@ualberta.ca>
2018-06-23 01:47:26 -0600

    Compile bigrams.

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,71 @@ use std::fs::File;
 use std::io::{BufReader, BufRead, Result};
 use std::collections::HashMap;
 
-const START: char = '\u{0002}'; // START OF TEXT
-const END: char = '\u{0003}'; // END OF TEXT
+#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
+enum Token {
+  Start,
+  End,
+  Char(char)
+}
 
 #[derive(PartialEq, Eq, Hash, Debug)]
-struct Bigram(char, char);
+struct Bigram(Token, Token);
 
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
     let crk_file = File::open("itwêwina").expect("file not found");
     let mut crk_bigrams = HashMap::new();
 
     for line in BufReader::new(crk_file).lines() {
         let line = line.expect("Couldn't get line");
-        count_bigrams(&mut crk_bigrams, line.trim())
+
+        count_bigrams(&mut crk_bigrams, &preprocess_line(&line));
     }
 
-    println!("{:?}", crk_bigrams);
+    for (bigram, count) in crk_bigrams.iter() {
+      println!("{0}\t{1}{2}", count, extract(bigram.0), extract(bigram.1));
+    }
     Ok(())
 }
 
-fn count_bigrams(counter: &mut HashMap<Bigram, i32>, text: &str) {
+fn extract(t: Token) -> char {
+  match t {
+    Token::Char(c) => c,
+    Token::Start => '^',
+    Token::End => '$'
+  }
+}
+
+/// Gets rid of surrounding whitespace,
+/// removes circumflexes,
+/// and lowercase's everting.
+fn preprocess_line(line: &str) -> String {
+  let mut buffer = String::new();
+  // Remove extraneous spaces and punctuation.
+  let word = line.trim_right_matches(|c| "!? \n".contains(c));
+
+  for ch in word.chars() {
+    let ch = ch.to_lowercase().nth(0).unwrap();
+    buffer.push(match ch {
+      'â' => 'a',
+      'ê' => 'e',
+      'î' => 'i',
+      'ô' => 'o',
+      _ => ch,
+    })
+  }
+
+  buffer
+}
+
+fn count_bigrams(counter: &mut HashMap<Bigram, i32>, text: &String) {
   if text.len() < 1 {
     return;
   }
 
-  let mut last_char = START;
-  for this_char in text.chars() {
+  let mut last_char = Token::Start;
+  for ch in text.chars() {
+    let this_char = Token::Char(ch);
     let bigram = Bigram(last_char, this_char);
 
     let count = counter.entry(bigram).or_insert(0);
@@ -37,4 +74,9 @@ fn count_bigrams(counter: &mut HashMap<Bigram, i32>, text: &str) {
 
     last_char = this_char;
   }
+
+  // Finalize by adding last character in the string.
+  let count = counter.entry(Bigram(last_char, Token::End))
+    .or_insert(0);
+  *count += 1;
 }
