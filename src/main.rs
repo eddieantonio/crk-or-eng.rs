085265616e4b9f0401a46042befbc0007348a0b7
Eddie Antonio Santos <easantos@ualberta.ca>
2018-06-25 04:08:38 -0600

    Refactor: Use enum instead of closure.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,6 @@ use std::fmt;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
-// Feature vector:
-// {has_ac: bool, has_ci: bool, has_im: bool, ...}
-// Represented as a sparce vector.
-//
-// In fact, storage can simply be a vector of digraphs present in a word.
-//
-// e.g.,
-// x = {^a, ac, ci, im, mo, si, is, s$} y = crk
-// x = {^p, pu, pp, py, y$}             y = eng
-//
-// Maybe invert this data structure?
-//
-// P(ci|crk) = P(count times in crk|counts overall)
-
 /**
  * Since we're interested in counting what are common starts of words, and common ends of words, a
  * "token" is more than simply a character---we encode the start and end of words explicitly.
@@ -33,6 +19,14 @@ enum Token {
  */
 #[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
 struct Digraph(Token, Token);
+
+/**
+ * Which language?
+ */
+enum Language {
+  Crk, // nêhiyawêwin/Plains Cree
+  Eng, // English
+}
 
 /**
  * How many times a digraph appears in nêhiyawêwin vs. English.
@@ -56,8 +50,7 @@ impl Classifier {
    * Given a filename, gets a set of all of the digraphs present in each word.
    * Use the "on_digraph" closure to increment the correct counter.
    */
-  fn count_digraphs_in_file<F>(&mut self, filename: &str, on_digraph: F)
-    where F: Fn(&mut Occurance) {
+  fn count_digraphs_in_file(&mut self, filename: &str, lang: Language) {
     let file = File::open(filename).expect("file not found");
 
     for line in BufReader::new(file).lines() {
@@ -66,7 +59,10 @@ impl Classifier {
       for digraph in digraphs_of(&word).iter() {
         let occ = self.features.entry(*digraph)
           .or_insert(Occurance { crk: 0, eng: 0});
-        on_digraph(occ)
+        match lang {
+          Language::Crk => occ.crk += 1,
+          Language::Eng => occ.eng += 1,
+        };
       }
     }
   }
@@ -85,8 +81,8 @@ impl Classifier {
 
 fn main() {
   let mut classifier = Classifier::new();
-  classifier.count_digraphs_in_file("itwêwina", |occ| { occ.crk += 1; });
-  classifier.count_digraphs_in_file("words", |occ| { occ.eng += 1; });
+  classifier.count_digraphs_in_file("itwêwina", Language::Crk);
+  classifier.count_digraphs_in_file("words", Language::Eng);
 
   classifier.prune_features();
 
